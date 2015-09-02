0ba95fc44d23ff66214b5c0eadde73053c64eeaa
marcker <marckfree@gmail.com>
2015-09-01 23:23:48 -0300

    Ajusta indentação

--- assets/plugins/microshare.js
+++ assets/plugins/microshare.js
@@ -1,44 +1,43 @@
 $(function() {
-  'use strict';
+    'use strict';
 
-  var openPopup;
+    var openPopup;
 
-  openPopup = function(e) {
-    e.preventDefault();
+    openPopup = function(e) {
+        e.preventDefault();
 
-    var shareService,
-      shareTitle,
-      shareUrl,
-      shareMap,
-      shareScheme;
+        var shareService,
+            shareTitle,
+            shareUrl,
+            shareMap,
+            shareScheme;
 
-    shareService = $(this).attr('data-microshare').toLowerCase(),
-    shareTitle = $(this).attr('data-microshare-title') || document.title,
-    shareUrl = $(this).attr('data-microshare-url') || document.URL,
+        shareService = $(this).attr('data-microshare').toLowerCase(),
+        shareTitle = $(this).attr('data-microshare-title') || document.title,
+        shareUrl = $(this).attr('data-microshare-url') || document.URL,
+        shareMap = [{
+            name: ['facebook', 'fb'],
+            link: 'https://www.facebook.com/sharer/sharer.php?u='
+        }, {
+            name: ['twitter', 'tw'],
+            link: 'https://twitter.com/home?status='
+        }, {
+            name: ['googleplus', 'gplus'],
+            link: 'https://plus.google.com/share?url='
+        }, {
+            name: ['linkedin', 'in'],
+            link: 'https://www.linkedin.com/shareArticle?mini=true&url=' + shareUrl + '&title=' + shareTitle + '&summary=&source='
+        }];
 
-		shareMap = [{
-      name: ['facebook', 'fb'],
-      link: 'https://www.facebook.com/sharer/sharer.php?u='
-    }, {
-      name: ['twitter', 'tw'],
-      link: 'https://twitter.com/home?status='
-    }, {
-      name: ['googleplus', 'gplus'],
-      link: 'https://plus.google.com/share?url='
-    }, {
-      name: ['linkedin', 'in'],
-      link: 'https://www.linkedin.com/shareArticle?mini=true&url=' + shareUrl + '&title=' + shareTitle + '&summary=&source='
-    }];
+        for (var x in shareMap) {
+            if (shareMap[x].name.indexOf(shareService) > -1) {
+                shareScheme = shareMap[x].link + shareUrl;
+                break;
+            }
+        }
 
-    for (var x in shareMap) {
-      if (shareMap[x].name.indexOf(shareService) > -1) {
-        shareScheme = shareMap[x].link + shareUrl;
-        break;
-      }
-    }
+        window.open(shareScheme, '_blank', 'height=400, width=800');
+    };
 
-    window.open(shareScheme, '_blank', 'height=400, width=800');
-  };
-
-  $(document).on('click', '[data-microshare]', openPopup);
+    $(document).on('click', '[data-microshare]', openPopup);
 });
